3988602ab3779b9db9070abeb871a0abc126fe15
Ajakaiye Afeso <marcusitovie@gmail.com>
2021-03-05 11:58:04 +0100

    Modify initial file

--- script.js
+++ script.js
@@ -1,20 +1,61 @@
-const quote = document.querySelector(".quote");
-const btn = document.querySelector(".btn");
+(function() {
+	const quoteBlock = document.querySelector("#quote-block");
+	const quote = document.querySelector(".quote");
+	const author = document.querySelector(".author");
+	const btn = document.querySelector(".btn");
 
-const quotes = ['“The Best Way To Get Started Is To Quit Talking And Begin Doing.” – Walt Disney',
-	'“The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.” – Winston Churchill',
-	'“Don’t Let Yesterday Take Up Too Much Of Today.” – Will Rogers',
-	'“You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.” – Unknown',
-	'“It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.” – Inspirational Quote By Vince Lombardi',
-	'“If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.” – Steve Jobs',
-	'“People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.” – Rob Siltanen',
-	'“Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough.” – Og Mandino',
-	'“Entrepreneurs Are Great At Dealing With Uncertainty And Also Very Good At Minimizing Risk. That’s The Classic Entrepreneur.” – Mohnish Pabrai',
-	'“We May Encounter Many Defeats But We Must Not Be Defeated.” – Maya Angelou'
-];
+	const quotes = [
+		{
+			quote: "“The Best Way To Get Started Is To Quit Talking And Begin Doing.”",
+			author: "Walt Disney"
+		},
+		{
+			quote: "“The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.”",
+			author: "Winston Churchill"
+		},
+		{
+			quote: "“Don’t Let Yesterday Take Up Too Much Of Today.”",
+			author: "Will Rogers"
+		},
+		{
+			quote: "“You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.”",
+			author: "Unknown"
+		},
+		{
+			quote: "“It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.”",
+			author: "Vince Lombardi"
+		},
+		{
+			quote: "“If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.”",
+			author: "Steve Jobs"
+		},
+		{
+			quote: "“People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.”",
+			author: "Rob Siltanen"
+		},
+		{
+			quote: "“Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough.”",
+			author: "Og Mandino"
+		},
+		{
+			quote: "“Entrepreneurs Are Great At Dealing With Uncertainty And Also Very Good At Minimizing Risk. That’s The Classic Entrepreneur.”",
+			author: "Mohnish Pabraio"
+		},
+		{
+			quote: "“We May Encounter Many Defeats But We Must Not Be Defeated.”",
+			author: "Maya Angelou"
+		}
+	];
 
-btn.addEventListener("click", function() {
 	const index = Math.floor(Math.random() * quotes.length);
-	quote.classList.add("quote-bg");
-	quote.textContent = quotes[index];
-});
+	quoteBlock.classList.add("quote-bg");
+	quote.textContent = quotes[index].quote;
+	author.textContent = quotes[index].author;
+
+	btn.addEventListener("click", function() {
+		const index = Math.floor(Math.random() * quotes.length);
+		quoteBlock.classList.add("quote-bg");
+		quote.textContent = quotes[index].quote;
+		author.textContent = quotes[index].author;
+	});
+})()
